0fcaa9144167f46be08f47d21975131e486354a2
Hao Chen <501018786@qq.com>
2024-10-13 12:19:36 +0800

    fix: add wrapper function to replace vim.bo and vim.wo

--- runtime/lua/vscode/sync-options.lua
+++ runtime/lua/vscode/sync-options.lua
@@ -15,6 +15,25 @@ local util = require("vscode.util")
 
 local M = {}
 
+-- Wrapper for nvim_set_option_value that sets the option only if the value differs
+function M.set_option_value(name, value, opts)
+  opts = opts or {}
+  local current = api.nvim_get_option_value(name, opts)
+  if current ~= value then
+    api.nvim_set_option_value(name, value, opts)
+  end
+end
+
+-- Wrapper for set_option_value to set a buffer option
+function M.buf_set_option(buf, name, value)
+  return M.set_option_value(name, value, { buf = buf })
+end
+
+-- Wrapper for set_option_value to set a window option
+function M.win_set_option(win, name, value)
+  return M.set_option_value(name, value, { win = win })
+end
+
 ---@class EditorOptions
 ---@field tabSize number
 ---@field insertSpaces boolean
@@ -31,77 +50,79 @@ end
 ---@param lineNumbers "on"|"off"|"relative"
 local function set_number(win, lineNumbers)
   if lineNumbers == "relative" then
-    vim.wo[win].rnu = true
+    M.win_set_option(win, "relativenumber", true)
   elseif lineNumbers == "on" then
-    vim.wo[win].nu = true
-    vim.wo[win].rnu = false
+    M.win_set_option(win, "number", true)
+    M.win_set_option(win, "relativenumber", false)
   else
-    vim.wo[win].nu = false
-    vim.wo[win].rnu = false
+    M.win_set_option(win, "number", false)
+    M.win_set_option(win, "relativenumber", false)
   end
 end
 
 ---Handle changes from vscode
 ---Set nvim options
 ---@param buf number
----@param new_opts EditorOptions
----@param old_opts EditorOptions
-local function set_options(buf, new_opts, old_opts)
+---@param opts EditorOptions
+local function set_options(buf, opts)
   if not api.nvim_buf_is_valid(buf) then
     return
   end
 
-  api.nvim_buf_set_var(buf, "vscode_editor_options", new_opts)
+  api.nvim_buf_set_var(buf, "vscode_editor_options", opts)
 
-  if new_opts.tabSize ~= old_opts.tabSize then
-    vim.bo[buf].ts = new_opts.tabSize
-    vim.bo[buf].sw = new_opts.tabSize
-  end
-
-  if new_opts.insertSpaces ~= old_opts.insertSpaces then
-    vim.bo[buf].et = new_opts.insertSpaces
-  end
+  M.buf_set_option(buf, "tabstop", opts.tabSize)
+  M.buf_set_option(buf, "shiftwidth", opts.tabSize)
+  M.buf_set_option(buf, "expandtab", opts.insertSpaces)
 
   local win = api.nvim_get_current_win()
-  if api.nvim_win_get_buf(win) == buf and new_opts.lineNumbers ~= old_opts.lineNumbers then
-    set_number(win, new_opts.lineNumbers)
+  if api.nvim_win_get_buf(win) == buf then
+    set_number(win, opts.lineNumbers)
   end
 end
 
 ---Check changes from nvim
 ---Set vscode options
 local function check_options()
-  local opts = vim.b.vscode_editor_options
-  if not opts then -- should not happen
+  local vscode_opts = vim.b.vscode_editor_options
+  if not vscode_opts then -- should not happen
     return
   end
 
   if not vim.b.vscode_editor_options_first_checked then --load the defaults
     vim.b.vscode_editor_options_first_checked = true
-    vim.bo.ts = opts.tabSize
-    vim.bo.sw = opts.tabSize
-    vim.bo.expandtab = opts.insertSpaces
-    set_number(0, opts.lineNumbers)
+    M.buf_set_option(0, "tabstop", vscode_opts.tabSize)
+    M.buf_set_option(0, "shiftwidth", vscode_opts.tabSize)
+    M.buf_set_option(0, "expandtab", vscode_opts.insertSpaces)
+    set_number(0, vscode_opts.lineNumbers)
     return
   end
 
-  local ts, sw, et = vim.bo.ts, vim.bo.sw, vim.bo.et
+  local buf = api.nvim_get_current_buf()
+  local win = api.nvim_get_current_win()
+  local ts = api.nvim_get_option_value("tabstop", { buf = buf })
+  local sw = api.nvim_get_option_value("shiftwidth", { buf = buf })
+  local et = api.nvim_get_option_value("expandtab", { buf = buf })
+
   if sw ~= ts then
-    vim.bo.sw = ts -- must be the same
+    M.buf_set_option(buf, "shiftwidth", ts) -- must be the same
   end
 
-  local new_opts = {
+  local nvim_opts = {
     tabSize = ts,
     insertSpaces = et,
-    lineNumbers = get_number_style(vim.wo.nu, vim.wo.rnu),
+    lineNumbers = get_number_style(
+      api.nvim_get_option_value("number", { win = win }),
+      api.nvim_get_option_value("relativenumber", { win = win })
+    ),
   }
 
-  if vim.deep_equal(new_opts, opts) then
+  if vim.deep_equal(nvim_opts, vscode_opts) then
     return
   end
 
-  vim.b.vscode_editor_options = new_opts
-  vscode.action("set_editor_options", { args = { api.nvim_get_current_buf(), new_opts } })
+  vim.b.vscode_editor_options = nvim_opts
+  vscode.action("set_editor_options", { args = { api.nvim_get_current_buf(), nvim_opts } })
 end
 
 local check_options_debounced = util.debounce(check_options, 20)
@@ -120,17 +141,14 @@ local function process_modeline()
 end
 
 function M.setup()
-  vscode.on("editor_options_changed", function(buf, new_opts)
-    local old_opts = vim.b[buf].vscode_editor_options or {}
-    set_options(buf, new_opts, old_opts)
-  end)
+  vscode.on("editor_options_changed", set_options)
   vscode.on("document_buffer_init", function(buf)
     if not api.nvim_buf_is_valid(buf) then
       return
     end
     local has, opts = pcall(api.nvim_buf_get_var, buf, "vscode_editor_options")
     if has then
-      set_options(buf, opts, {})
+      set_options(buf, opts)
       vim.defer_fn(process_modeline, 100)
     end
   end)
